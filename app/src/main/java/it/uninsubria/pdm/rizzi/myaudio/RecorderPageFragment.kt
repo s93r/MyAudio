06d9fca4689c2488f9c30c1770e07a9a2fa4925d
sr <silvio.rizzi93@gmail.com>
2022-06-17 18:40:54 +0200

    navigation graph update

--- app/src/main/java/it/uninsubria/pdm/rizzi/myaudio/RecorderPageFragment.kt
+++ app/src/main/java/it/uninsubria/pdm/rizzi/myaudio/RecorderPageFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_recorder_page.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +39,15 @@ class RecorderPageFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_recorder_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val controller: NavController = Navigation.findNavController(view)
+        val list_btn: ImageView = iv_list
+        list_btn.setOnClickListener {
+            controller.navigate(R.id.action_recorderPageFragment_to_recordingListFragment)
+        }
     }
 
     companion object {
