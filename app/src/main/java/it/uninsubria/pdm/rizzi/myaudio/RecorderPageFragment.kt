400a28a2f679c4f529f9d90e17d2a9cabda20075
sr <silvio.rizzi93@gmail.com>
2022-06-20 18:06:51 +0200

    fragment recorder page : audio recording implementation v.1

--- app/src/main/java/it/uninsubria/pdm/rizzi/myaudio/RecorderPageFragment.kt
+++ app/src/main/java/it/uninsubria/pdm/rizzi/myaudio/RecorderPageFragment.kt
@@ -1,15 +1,24 @@
 package it.uninsubria.pdm.rizzi.myaudio
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_recorder_page.*
+import java.io.IOException
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +34,9 @@ class RecorderPageFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private var recorder: MediaRecorder? = null
+    //val recorder: MediaRecorder = MediaRecorder()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,22 +57,65 @@ class RecorderPageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val controller: NavController = Navigation.findNavController(view)
+
         val rec_btn: Button = btn_rec
         val list_img: ImageView = iv_playlist
         var isRecording: Boolean = false
         rec_btn.setOnClickListener {
             if (isRecording) {
+                stopRecording()
                 rec_btn.backgroundTintList = resources.getColorStateList(android.R.color.holo_blue_light, null)
                 rec_btn.text = "REC: OFF"
                 isRecording = false
             } else {
-                rec_btn.backgroundTintList = resources.getColorStateList(android.R.color.holo_red_light, null)
-                rec_btn.text = "REC: ON"
-                isRecording = true
+                if (hasPermission()) {
+                    startRecording()
+                    rec_btn.backgroundTintList = resources.getColorStateList(android.R.color.holo_red_light, null)
+                    rec_btn.text = "REC: ON"
+                    isRecording = true
+                }
             }
         }
         list_img.setOnClickListener {
             controller.navigate(R.id.action_recorderPageFragment_to_recordingListFragment)
+        }
+    }
+
+    private fun stopRecording() {
+        recorder?.stop()
+        recorder?.release()
+    }
+
+    private fun startRecording() {
+
+        val path = Environment.getExternalStorageDirectory().absolutePath
+        val file = "test3.mp3"
+        recorder = MediaRecorder()
+        recorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+        recorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+
+        recorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+        recorder?.setOutputFile(path+"/"+file)
+        try {
+            recorder?.prepare()
+            recorder?.start()
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    private fun hasPermission(): Boolean {
+        if (ContextCompat.checkSelfPermission(context!!, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(context!!, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            return true
+        } else {
+            val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            ActivityCompat.requestPermissions(activity!!, permissions,0)
+            //ActivityCompat.requestPermissions(activity!!, arrayOf(Manifest.permission.RECORD_AUDIO), 17)
+            return false
         }
     }
 
