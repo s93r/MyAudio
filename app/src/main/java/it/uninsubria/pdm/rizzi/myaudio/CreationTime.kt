5afcc0b49b102decf4ba7288c3680b674861f152
sr <silvio.rizzi93@gmail.com>
2022-07-05 19:04:29 +0200

    creation time : code cleanup v.1

--- app/src/main/java/it/uninsubria/pdm/rizzi/myaudio/CreationTime.kt
+++ app/src/main/java/it/uninsubria/pdm/rizzi/myaudio/CreationTime.kt
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit
 class CreationTime {
 
     fun timeOfCreation(duration: Long) : String {
+
         val date: Date = Date()
 
         val seconds = TimeUnit.MILLISECONDS.toSeconds(date.time - duration)
@@ -17,17 +18,16 @@ class CreationTime {
             return "created just now"
         } else if (minutes == 1L) {
             return "created a minute ago"
-        } else if (minutes > 1 && minutes < 60) {
-            return "created " + minutes + " minutes ago"
+        } else if (minutes in 2..59) {
+            return "created $minutes minutes ago"
         } else if (hours == 1L) {
             return "created an hour ago"
-        } else if (hours > 1 && hours < 24) {
-            return "created " + hours + " hours ago"
+        } else if (hours in 2..23) {
+            return "created $hours hours ago"
         } else if (days == 1L) {
             return "created a day ago"
         } else {
-            return "created " + days + " days ago"
+            return "created $days days ago"
         }
     }
-
 }
